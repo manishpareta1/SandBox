3bffbf30bfb7feebe5401c4b8624ab0f7919d49b
manishpareta1 <manishpareta1@gmail.com>
2019-09-20 18:46:07 +0530

    added further funtion to make generalize the implementation of fixed and incremental cost calculation, this is help us in add different kind of cost to calculate

--- Java8Basic/src/com/java8/pluralsightExample/FunctionOvertime.java
+++ Java8Basic/src/com/java8/pluralsightExample/FunctionOvertime.java
@@ -12,10 +12,23 @@ interface FunctionOvertime {
     }
 
     static FunctionOvertime fixedCost(final double value){
-        return (time) -> value;
+        //return (time) -> value;
+        return FunctionOvertime.polynomial(new double[]{value});
     }
 
     static FunctionOvertime incrementalCost(final double intercept, final double slop){
-        return (time) -> intercept + slop*time;
+        //return (time) -> intercept + slop*time;
+        return FunctionOvertime.polynomial(new double[]{intercept,slop});
+    }
+
+    //adding polynomial function to make fixed cost and incremental cost use a more generic way of implementation
+    static FunctionOvertime polynomial(final double[] values){
+        return (time) ->{
+            Double sum = 0.0;
+            for(int i=0; i<values.length;i++){
+                sum += Math.pow(time, i)*values[i];
+            }
+            return sum;
+        };
     }
 }
